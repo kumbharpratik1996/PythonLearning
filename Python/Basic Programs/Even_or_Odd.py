c7002585d45e1e6eb65d34fd5a1562692a3343a0
Pratik Kumbhar <Pratik.Kumbhar@datametica.com>
2023-05-26 16:43:54 +0530

    moving basic program to a subfolder

---
+++ Python/Basic Programs/Even_or_Odd.py
@@ -0,0 +1,7 @@
+num = int(input("Please enter a value: \n"))
+print("You have entered " + str(num) + "\n Please wait to check whether number is even or odd!")
+
+if num % 2 == 0:
+    print("Entered number is even")
+else:
+    print("Entered number is odd")
